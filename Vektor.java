8bca16bae11dac9017e5fdd6e0fd9945f7641837
Max Danigel <maxdanigel1204@gmail.com>
2026-04-10 00:10:30 +0200

    Eingefügt von beiden Vektoren Konstruktoren.

--- Vektor.java
+++ Vektor.java
@@ -18,13 +18,33 @@ public class Vektor {
 	/**
 	 * laenge entspricht wie lang ein Vektor ist.
 	 */
-	private float laenge;
+	private double laenge;
 	/**
 	 * winkel entspricht in welche Richtung der Vektor geht.
 	 */
-	private float winkel;
+	private double winkel;
 	/**
 	 * NULLPUNKT entspricht den Konstanten Ursprung.
 	 */
 	public static final float NULLPUNKT = 0.0f;
+
+	/**
+	 * @param x
+	 * @param y
+	 * @throws IllegalArgumentException damit x und y um zu verhindern das beide mit 0 belegt werden.
+	 */
+	public Vektor(int x, int y)throws IllegalArgumentException{
+		if(x == 0 && y == 0){
+			throw new IllegalArgumentException("x und y dürfen nicht beide 0 sein!");
+		}
+		this.x = x;
+		this.y = y;
+	}
+	public Vektor(int x, int y, double winkel)throws IllegalArgumentException{
+		this(x, y);
+		if(){
+
+		}
+		laenge = Math.sqrt(x * x + y * y);
+	}
 }
