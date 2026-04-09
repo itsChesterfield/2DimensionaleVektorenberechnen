837ca5f18b15f34536da539f63dd54dec1cc8a19
Max Danigel <maxdanigel1204@gmail.com>
2026-04-09 23:54:03 +0200

    Kommentare hinzugefügt.

--- Vektor.java
+++ Vektor.java
@@ -1,9 +1,30 @@
+
 package mathematik;
 
+/**
+ * Repräsentiert einen 2 Dimensionalen Vektor.
+ * @Author Max Danigel.
+ * @version 1.0
+ */
 public class Vektor {
+	/**
+	 * x entspricht den Wert der Horizontalen Ebene.
+	 */
 	private int x;
+	/**
+	 * y entspricht den Wert der Vertikalen Ebene
+	 */
 	private int y;
+	/**
+	 * laenge entspricht wie lang ein Vektor ist.
+	 */
 	private float laenge;
+	/**
+	 * winkel entspricht in welche Richtung der Vektor geht.
+	 */
 	private float winkel;
+	/**
+	 * NULLPUNKT entspricht den Konstanten Ursprung.
+	 */
 	public static final float NULLPUNKT = 0.0f;
 }
