9073b0ebb923403a401a2827c724c49655832748
Max Danigel <maxdanigel1204@gmail.com>
2026-04-10 09:16:34 +0200

    imports und @BeforeEach in VektorTest

--- VektorTest.java
+++ VektorTest.java
@@ -1,4 +1,11 @@
 package mathematik;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
 
 public class VektorTest {
+	@BeforeEach
+	public void objektErstellen(){
+		Vektor vik = new Vektor(10.2, 32.1);
+	}
+
 }
